127ce7fe85cceb638aed8f9b62636065b39b7a20
Shadab Sutar <shadab.sutar@gmail.com>
2018-08-06 23:10:23 +0530

    Cleansing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+//React MDL Components Import
+import {Layout, Header, Navigation, Drawer, Content,
+        Card, CardTitle, CardText, CardActions, CardMenu,
+        Button, IconButton, Textfield } from 'react-mdl';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="demo-big-content">
+          <Layout>
+              <Header title="eSplitz" scroll>
+                  <Navigation>
+                      <a href="#">About</a>
+                      <a href="#">Policies</a>
+                      <a href="#">Sign Up</a>
+                      <a href="#">Login</a>
+                  </Navigation>
+              </Header>
+              <Drawer title="eSplitz">
+                  <Navigation>
+                      <a href="#">About</a>
+                      <a href="#">Policies</a>
+                      <a href="#">Sign Up</a>
+                      <a href="#">Login</a>
+                  </Navigation>
+              </Drawer>
+              <Content>
+                  <div className="page-content">
+                  <Card shadow={0} style={{width: '70%', margin: 'auto'}}>
+                      <CardTitle style={{color: '#fff', height: '200px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome</CardTitle>
+                      <CardText>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                          Mauris sagittis pellentesque lacus eleifend lacinia...
+                      </CardText>
+                      <CardActions border>
+                          <Textfield
+                              onChange={() => {}}
+                              label="Username"
+                              floatingLabel
+                              style={{width: '100%'}}
+                          />
+                          <Textfield
+                              onChange={() => {}}
+                              label="Password"
+                              floatingLabel
+                              style={{width: '100%'}}
+                          />
+                          <Button colored>Login</Button>
+                      </CardActions>
+                      <CardMenu style={{color: '#fff'}}>
+                          <IconButton name="share" />
+                      </CardMenu>
+                  </Card>
+                  </div>
+              </Content>
+          </Layout>
+      </div>
       </div>
     );
   }
